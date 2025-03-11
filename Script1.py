2879728af755be0c02b4d0acb4d58532cbf6f372
Andres Aguilar Estrada <alejandro.aguilar39@uabc.edu.mx>
2025-03-11 10:33:14 -0700

    Modificamos el Script1.py y Script2.py en la rama nueva-funcionalidad

--- Script1.py
+++ Script1.py
@@ -1,1 +1,2 @@
 print("Hola desde Script 1")
+#Agregamos una nueva rama para una nueva funcionlaidad a este script
---
+++ Script2.py
@@ -0,0 +1,2 @@
+print("Hola desde Scrit2")
+#Agregamos una nueva rama para una nueva funcionlaidad a este script tambien 
